4c0764ba3fd63af8f48b80accdb66ec5c0a43e7b
LetsChill <undefined@example.com>
2023-06-07 12:59:00 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="pikanetwork.py",
+    version="0.0.1",
+    author="LetsChill",
+    description="An api wrapper for pikanetwork",
+    packages=["pikanetwork"]
+)
